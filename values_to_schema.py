99121d3f8cb0ddf89544e51c43a4007b2fbe603b
Carlos Fernandez Musoles <carlos@kalavai.net>
2026-01-25 12:22:39 +0000

    added pool template

--- values_to_schema.py
+++ values_to_schema.py
@@ -6,7 +6,7 @@ import yaml
 import json
 
 
-TEMPLATE = "llamacpp"
+TEMPLATE = "pool"
 INPUT_VALUES_FILE = f"charts/{TEMPLATE}/values.yaml"
 INPUT_METADATA_FILE = f"charts/{TEMPLATE}/metadata.json"
 OUTPUT_SCHEMA_FILE = f"charts/{TEMPLATE}/values.schema.json"

901e61909e28aedd67c80170802d9e470362e898
Carlos Fernandez Musoles <carlos@kalavai.net>
2026-02-08 17:23:54 +0000

    roll back vllm latest version due to bug with numpy

--- charts/vllm/build_images.sh
+++ charts/vllm/build_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PIP_VLLM_VERSION=${1:-"0.11.2"}
+PIP_VLLM_VERSION=${1:-"0.12.0"}
 
 echo "Building vLLM version: $PIP_VLLM_VERSION"
 
@@ -10,13 +10,13 @@ docker push ghcr.io/kalavai-net/vllm-cuda:v$PIP_VLLM_VERSION
 
 # ROCm
 # build base image first
-git clone https://github.com/vllm-project/vllm.git
-cd vllm
-git checkout releases/v$PIP_VLLM_VERSION
-DOCKER_BUILDKIT=1 docker build --build-arg BUILD_FA="0" -f docker/Dockerfile.rocm -t ghcr.io/kalavai-net/vllm-rocm-base:v$PIP_VLLM_VERSION .
-docker push ghcr.io/kalavai-net/vllm-rocm-base:v$PIP_VLLM_VERSION
-cd ..
-rm -rf vllm
+# git clone https://github.com/vllm-project/vllm.git
+# cd vllm
+# git checkout releases/v$PIP_VLLM_VERSION
+# DOCKER_BUILDKIT=1 docker build --build-arg BUILD_FA="0" -f docker/Dockerfile.rocm -t ghcr.io/kalavai-net/vllm-rocm-base:v$PIP_VLLM_VERSION .
+# docker push ghcr.io/kalavai-net/vllm-rocm-base:v$PIP_VLLM_VERSION
+# cd ..
+# rm -rf vllm
 
-docker build --build-arg VLLM_VERSION=v$PIP_VLLM_VERSION -t ghcr.io/kalavai-net/vllm-rocm:v$PIP_VLLM_VERSION -f src/Dockerfile_rocm src/
-docker push ghcr.io/kalavai-net/vllm-rocm:v$PIP_VLLM_VERSION
+# docker build --build-arg VLLM_VERSION=v$PIP_VLLM_VERSION -t ghcr.io/kalavai-net/vllm-rocm:v$PIP_VLLM_VERSION -f src/Dockerfile_rocm src/
+# docker push ghcr.io/kalavai-net/vllm-rocm:v$PIP_VLLM_VERSION
